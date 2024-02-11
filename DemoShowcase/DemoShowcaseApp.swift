50f524b7e353161e6e4a68c971680a2e3b185556
Iustin Bulimar <iustin.bulimar@gmail.com>
2024-02-11 13:07:11 +0200

    Initial commit

---
+++ DemoShowcase/DemoShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoShowcaseApp.swift
+//  DemoShowcase
+//
+//  Created by Iustin Bulimar on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
